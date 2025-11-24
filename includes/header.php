f84911f65f5df302ecf5a58fa6bef53eae2fe71b
Batman <lucas.parente0808@gmail.com>
2025-11-24 16:54:17 -0300

    Correção de bugs e novo visual

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,9 @@ if (!isset($page_title)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/forum/assets/css/style.css">
 </head>
 <body>
